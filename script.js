2c8fcb0ada23c6d361998d4d7fc37f606e0a7dcb
Aliyabegum30 <seheraliya786@gmail.com>
2026-03-11 22:12:28 +0530

    Weather dashboard with API integration

--- script.js
+++ script.js
@@ -2,24 +2,41 @@ async function getWeather(){
 
  const city = document.getElementById("city").value;
 
+ if(city === ""){
+  document.getElementById("weather").innerHTML =
+  "Please enter a city name";
+  return;
+ }
+
  const apiKey = "5f11a2a8bb4b0b11c3e5cc47f93aecf6";
 
  const url =
  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
- const response = await fetch(url);
+ document.getElementById("weather").innerHTML = "Loading weather...";
 
- const data = await response.json();
+ try{
 
- if(data.cod == "404"){
-  document.getElementById("weather").innerHTML = "City not found";
-  return;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if(data.cod !== 200){
+   document.getElementById("weather").innerHTML =
+   "City not found";
+   return;
+  }
+
+  document.getElementById("weather").innerHTML = `
+  <h2>${data.name}</h2>
+  <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+  <p>Temperature: ${data.main.temp} °C</p>
+  <p>Humidity: ${data.main.humidity}%</p>
+  <p>Weather: ${data.weather[0].description}</p>
+  `;
+
+ }catch(error){
+  document.getElementById("weather").innerHTML =
+  "Error fetching weather data";
  }
 
- document.getElementById("weather").innerHTML = `
-   <h2>${data.name}</h2>
-   <p>Temperature: ${data.main.temp} °C</p>
-   <p>Humidity: ${data.main.humidity}%</p>
-   <p>Weather: ${data.weather[0].description}</p>
- `;
 }
